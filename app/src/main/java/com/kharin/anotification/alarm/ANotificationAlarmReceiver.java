dd8dec8b38776911b12e2b381156510273c5d4a3
Kharindev <kharindev@gmail.com>
2022-09-18 18:19:50 +0300

    v4

--- app/src/main/java/com/kharin/anotification/alarm/ANotificationAlarmReceiver.java
+++ app/src/main/java/com/kharin/anotification/alarm/ANotificationAlarmReceiver.java
@@ -4,8 +4,10 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
 import com.kharin.anotification.ANotificationSettings;
 import com.kharin.anotification.ANotificationManager;
 import com.kharin.anotification.R;
